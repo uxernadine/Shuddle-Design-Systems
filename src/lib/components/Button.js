c3d43506fe1ce6e145f31fefa4cafc6f26601865
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-11 07:26:58 +0000

    fixed up typography tokens

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import designTokens from './tokens.json';
 var color = designTokens.global.color;
-  
+
 const Button = (props) => {  
   return (
     <button className="button"
       style={{
-        backgroundColor: color.primary.default.value,
+        backgroundColor: color.primary.travel.default.value,
         color: color.neutral.default.value,
-        fontFamily: designTokens.global['H4-Bold'].value.fontFamily,
-        fontSize: designTokens.global['H4-Bold'].value.fontSize,
+        fontSize: designTokens.global.fontSize.h4.value,
         border: 'none',
         padding: '16px 8px',
         borderRadius: '8px'
