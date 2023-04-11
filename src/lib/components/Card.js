a96d20398a08797ace91ad79bf6d8de47c8a2180
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-11 17:15:17 +0000

    removed h2 in order to use design tokens instead

--- src/lib/components/Card.js
+++ src/lib/components/Card.js
@@ -6,8 +6,10 @@ const Card = (props) => {
       <card className="card">
           <img src={props.image} alt={props.title} />
           <div className="CardContent">
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            {props.title}
+            <p>
+              {props.description}
+            </p>
             <Button label="Read More" />
           </div>
           {props.label}
