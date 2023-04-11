cb660d48058ac202c38cf042767beec2e8be75c3
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-11 17:30:45 +0000

    added styling to card component

--- src/lib/components/Card.js
+++ src/lib/components/Card.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Button from './Button';
+import designTokens from './tokens.json';
 
 const Card = (props) => {  
     return (
-      <card className="card">
+      <card className="card"
+        style={{
+          borderRadius: designTokens.global.borderRadius.value,
+          backgroundColor: designTokens.global.color.primary.rail.default
+        }}
+      >
           <img src={props.image} alt={props.title} />
-          <div className="CardContent">
+          <div className="CardBody">
             {props.title}
             <p>
               {props.description}
