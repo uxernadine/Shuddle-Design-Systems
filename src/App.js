e6008f4ea1e25224a3cf79067d43b904cc4b0a56
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-13 08:16:04 +0000

    added spacing between the logo and card component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
             <img src="shuddleLogoDark.png" className="App-logo" alt="shuddle logo" />
         <p>
-          Shuddle Design System Pilot
+          
         </p>
         <Card
           image="https://a0.muscache.com/im/pictures/miso/Hosting-28254684/original/99bd44d1-abca-4b1c-b5da-eb05eaac9193.jpeg?im_w=720"
