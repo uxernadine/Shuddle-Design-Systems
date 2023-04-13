9f86fe01f25e69027dce560ce0ee160c5a5c0f82
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-13 01:58:04 -0500

    Update Card.js

--- src/lib/components/Card.js
+++ src/lib/components/Card.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import Button from './Button';
-import designTokens from './tokens.json';
-
-/* const Card = (props) => {  
-    return (
-      <card className="card"
-        style={{
-          borderRadius: designTokens.global.borderRadius.value,
-          backgroundColor: designTokens.global.color.primary.rail.default
-        }}
-      >
-          <img src={props.image} alt={props.title} />
-          <div className="CardBody">
-            {props.title}
-            <p>
-              {props.description}
-            </p>
-            <Button label="Read More" />
-          </div>
-          {props.label}
-      </card>
-    )
-  
-  }
-
-  export default Card;
-  */
 
   const Card = (props) => {  
     return (
