01cb7c5a942b0ba5db2effb49cb1be098431fa1c
uxernadine <126738437+uxernadine@users.noreply.github.com>
2023-04-14 23:42:21 +0000

    added more style to the IPTS theme

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import './App.css';
 import Card from './lib/components/Card'
 
@@ -5,10 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-            <img src="shuddleLogoDark.png" className="App-logo" alt="shuddle logo" />
-        <p>
-          
-        </p>
+          <img src="planet.svg" className="App-logo" alt="IPTS logo" />
+          <img src="ipts.svg" alt="IPTS title" />
+        <Card
+          image="https://a0.muscache.com/im/pictures/miso/Hosting-28254684/original/99bd44d1-abca-4b1c-b5da-eb05eaac9193.jpeg?im_w=720"
+          title="IPTS Stay"
+          description="Explore the galaxy in a new way with IPTS"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+import './App.css';
+import Card from './lib/components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <img src="shuddleLogoDark.png" className="App-logo" alt="Shuddle Logo" />
         <Card
           image="https://a0.muscache.com/im/pictures/miso/Hosting-28254684/original/99bd44d1-abca-4b1c-b5da-eb05eaac9193.jpeg?im_w=720"
           title="Shuddle Stay"
